26402811a95f4be617861ae87895338e77ace33b
kukanji <111495470+kukanji@users.noreply.github.com>
2024-11-06 22:48:15 +0900

    prismaに書き換えた

--- next-app/auth.ts
+++ next-app/auth.ts
@@ -3,16 +3,16 @@ import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import bcrypt from "bcrypt";
 import { z } from "zod";
-import prisma from './db'
-import { User } from '@prisma/client';
- 
+import prisma from "./db";
+import { User } from "@prisma/client";
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await prisma.user.findUnique({ where: { email: email } });
+    return user;
   } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
   }
 }
 
@@ -29,17 +29,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
+
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
- 
+
           if (passwordsMatch) return user;
         }
- 
-        console.log('Invalid credentials');
+
+        console.log("Invalid credentials");
         return null;
       },
     }),
